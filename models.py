30cb8e9362672d18bb5c3c91ca2dfd645fafbcc8
Isaiah <isaiaht8t@gmail.com>
2018-07-23 10:38:58 -0400

    models update
    models update

--- models.py
+++ models.py
@@ -3,4 +3,8 @@ from google.appengine.ext import ndb
 class User(ndb.Model):
     user_name = ndb.StringProperty(required=True)
     comment = ndb.FloatProperty(required=True)
-    
+
+
+class Store(ndb.Model):
+    title = ndb.StringProperty()
+    url = ndb.StringProperty()
